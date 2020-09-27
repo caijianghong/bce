e81b2ef7dc707c2724ce3460a256ce9cb2d786aa
蔡江洪 <278436264@qq.com>
2020-09-27 16:21:33 +0800

    update： sss

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,7 @@ const resolve = dir => {
 const BASE_URL = '/'
 module.exports={
     // publicPath: process.env.NODE_ENV === "production" ? "/bce" : BASE_URL,
-    publicPath:"./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
-    outputDir:"dist",  //打包时生成的生产环境构建文件的目录
-    assetsDir: 'public', 
+    publicPath:"",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
     lintOnSave: true,
     productionSourceMap: true,
     chainWebpack: config => {
